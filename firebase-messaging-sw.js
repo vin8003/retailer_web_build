7ca7b5a3377d5745095ef4ddd05b440df2237c69
vin8003 <vin8003@gmail.com>
2025-12-31 00:08:39 +0530

    initial build

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
+firebase.initializeApp({
+    apiKey: "AIzaSyDL43Wm3WJwn7DysDb6kCfT425riQ7bytw",
+    authDomain: "buyeasy-4003f.firebaseapp.com",
+    projectId: "buyeasy-4003f",
+    storageBucket: "buyeasy-4003f.firebasestorage.app",
+    messagingSenderId: "266120844334",
+    appId: "1:266120844334:web:0d9ba7f821b7447b9ebbbf",
+    measurementId: "G-NYWF0GNCZ5"
+});
+const messaging = firebase.messaging();
